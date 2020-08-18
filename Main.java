3dec538390d7c38e797899cda3bf6a7df7a04a44
Xenia Bocharova <bochxenia@yandex.ru>
2020-08-18 19:46:19 +0300

    Добавлен код

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+int score = 2_000_000_000;
+int transferAmount = 500_000_000;
+int balance = score + transferAmount;
+System.out.println(balance);
+    }
+}
